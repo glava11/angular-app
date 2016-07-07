44acf3205fbd057ace23dd1e27ee2f09ea5b2166
Drazen Orsolic <dorsolic@yelsterdigital.com>
2016-07-07 09:11:39 +0200

    db configuration

--- server/lib/initDB.js
+++ server/lib/initDB.js
@@ -2,7 +2,7 @@
 var rest = require('request');
 
 var initDB = {
-  adminUser: { email: 'admin@abc.com', password: 'changeme', admin: true, firstName: 'Admin', lastName: 'User' },
+  adminUser: { email: 'glava11@gmail.com', password: 'MongoChao', admin: true, firstName: 'Admin', lastName: 'User' },
 
   initialize: function(cfg) {
     initDB.apiKey = cfg.mongo.apiKey;
